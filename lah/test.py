d03334c58684ea8520f0f935ae3f23863cea7a48
Rajiv Sambharya <rajivsambharya@Rajivs-MacBook-Pro-2.local>
2025-04-15 13:44:46 -0400

    fixed error with disable jit

--- lah/test.py
+++ lah/test.py
@@ -1,6 +1,8 @@
 from pep import build_A_matrix_with_xstar
 
 import jax.numpy as jnp
+import jax
+jax.config.update('jax_disable_jit', True)
 
 alpha = jnp.array([0.1, 0.2, 0.3])
 beta = jnp.array([0.4, 0.5, 0.6])
