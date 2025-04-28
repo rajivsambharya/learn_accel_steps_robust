b798dc03def16dba29f737ed74e5a5e8c860202a
Rajiv Sambharya <rajivsambharya@gmail.com>
2025-04-28 16:56:09 -0400

    minor modifications to test_layers

--- lah/test_layers.py
+++ lah/test_layers.py
@@ -52,7 +52,8 @@ def test_smooth_nesterov_pep_layer_matches_pepit(vanilla_setup):
 
     # Assert that the two values are close
     # np.testing.assert_allclose(our_tau, pepit_tau, rtol=1e-3, atol=1e-6)
-    np.isclose(our_tau, pepit_tau, rtol=1e-4, atol=1e-6)
+    # np.testing.assert_allclose(our_tau, pepit_tau, rtol=1e-4, atol=1e-6)
+    np.testing.assert_allclose(our_tau, pepit_tau, atol=1e-4)
 
 def test_smooth_nesterovprox_pep_layer_matches_pepit(proxgd_setup):
     mu, L, num_iter, A, alpha, beta = proxgd_setup
@@ -68,13 +69,14 @@ def test_smooth_nesterovprox_pep_layer_matches_pepit(proxgd_setup):
 
     # Assert that the two values are close
     # np.testing.assert_allclose(our_tau, pepit_tau, rtol=1e-3, atol=1e-6)
-    np.isclose(our_tau, pepit_tau, rtol=1e-4, atol=1e-6)
+    # np.testing.assert_allclose(our_tau, pepit_tau, rtol=1e-4, atol=1e-6)
+    np.testing.assert_allclose(our_tau, pepit_tau, atol=1e-4)
 
 def test_strcvx_smooth_quad_pep_layer_matches_pepit(vanilla_setup):
     mu, L, num_iter, A, alpha, beta = vanilla_setup
 
     layer_quad = create_quad_pep_sdp_layer(mu, L, num_iter)
-    G = layer_quad(A, solver_args={"verbose": True})
+    G = layer_quad(A, solver_args={"eps": 1e-5, "verbose": True})
     our_tau = float(G[0][2*num_iter + 2, 2*num_iter + 2])
 
     pepit_tau = float(pepit_accel_gd(mu, L, np.column_stack((alpha, beta)), True, False, "dist"))
@@ -82,13 +84,13 @@ def test_strcvx_smooth_quad_pep_layer_matches_pepit(vanilla_setup):
     print("Our tau:", our_tau)
     print("PEPit tau:", pepit_tau)
 
-    np.isclose(our_tau, pepit_tau, rtol = 1e-4, atol = 1e-6)
+    np.testing.assert_allclose(our_tau, pepit_tau, rtol = 1e-4, atol = 1e-6)
 
 def test_strcvx_smooth_quadprox_pep_layer_matches_pepit(proxgd_setup):
     mu, L, num_iter, A, alpha, beta = proxgd_setup
 
     layer_quadprox = create_quadprox_pep_sdp_layer(mu, L, num_iter)
-    G, H = layer_quadprox(A, solver_args={"verbose": True})
+    G, H = layer_quadprox(A, solver_args={"eps": 1e-5, "verbose": True})
     our_tau = float(G[3*num_iter + 7, 3*num_iter + 7] - 2 * G[3*num_iter + 7, 1] + G[1, 1])
 
     pepit_tau = float(pepit_accel_gd(mu, L, np.column_stack((alpha, beta)), True, True, "dist"))
@@ -96,4 +98,5 @@ def test_strcvx_smooth_quadprox_pep_layer_matches_pepit(proxgd_setup):
     print("Our tau:", our_tau)
     print("PEPit tau:", pepit_tau)
 
-    np.isclose(our_tau, pepit_tau, rtol = 1e-4, atol = 1e-6)
+    # np.testing.assert_allclose(our_tau, pepit_tau, rtol = 1e-2, atol = 1e-4)
+    np.testing.assert_allclose(our_tau, pepit_tau, atol = 1e-4)
